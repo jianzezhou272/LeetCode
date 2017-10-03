67ca77b5be9c953fc3537f574e6dfa6cba800e19
jianzezhou272 <32252452+jianzezhou272@users.noreply.github.com>
2017-10-02 21:48:07 -0400

    Add files via upload

---
+++ 26.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i=0
+        n=len(nums)
+        while i<n-1:
+            if nums[i]==nums[i+1]:
+                nums.pop(i+1)
+                n-=1
+            else:
+                i+=1
+        return n
