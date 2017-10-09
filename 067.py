4bddd1b2409d10802663588bef34071fd9bb865a
jianzezhou272 <32252452+jianzezhou272@users.noreply.github.com>
2017-10-09 15:46:06 -0400

    Add files via upload

---
+++ 067.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        return bin(int(b,2)+int(a,2))[2:]
