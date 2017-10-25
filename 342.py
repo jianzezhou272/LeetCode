b1ea41f2a9c7be9f7196a38d5025c5226dd0adde
jianzezhou272 <32252452+jianzezhou272@users.noreply.github.com>
2017-10-24 21:49:52 -0400

    Add files via upload

---
+++ 342.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPowerOfFour(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        while num>1:
+            if num%4!=0:
+                return False
+            else:
+                num=num/4
+        return num==1
