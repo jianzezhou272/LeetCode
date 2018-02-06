c5c4b46c99fbc12ccde000fdd17a4c69c33ec076
jianzezhou272 <32252452+jianzezhou272@users.noreply.github.com>
2018-02-05 22:13:19 -0500

    Add files via upload

---
+++ 657.py
@@ -0,0 +1,9 @@
+from collections import Counter
+class Solution(object):
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        c=Counter(moves)
+        return c['U']==c['D'] and c['L']==c['R']
